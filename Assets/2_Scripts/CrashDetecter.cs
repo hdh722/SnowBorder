3ebb79745919df73e22041e8149b49578ee399b0
hdh <vahyeon2512@sonline20.sen.go.kr>
2025-05-23 15:20:15 +0900

    40까지 완료

--- Assets/2_Scripts/CrashDetecter.cs
+++ Assets/2_Scripts/CrashDetecter.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class CrashDetecter : MonoBehaviour
 {
+    [SerializeField] private float delay = 1f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
         {
             Debug.Log("Ãæµ¹");
-            SceneManager.LoadScene(0);
+            Invoke(nameof(ReloadScene), delay);
         }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/2_Scripts/FinishLine.cs
+++ Assets/2_Scripts/FinishLine.cs
@@ -2,12 +2,32 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField]
+    private float delay = 2f;
+    private ParticleSystem finishEffect;
+    void Awake()
+    {
+        // 하위 오브젝트에서 FinishEffect 파티클 시스템을 찾음
+        Transform effectTransform = transform.Find("FinishEffect");
+        if (effectTransform != null)
+        {
+            finishEffect = effectTransform.GetComponent<ParticleSystem>();
+        }
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("player 완주");
-            SceneManager.LoadScene(0);
+            if (finishEffect != null)
+            {
+                finishEffect.Play();
+            }
+            Invoke(nameof(ClearReloadScene), delay);
         }
+    }
+    void ClearReloadScene()
+    {
+        SceneManager.LoadScene("Level1");
     }
 }
